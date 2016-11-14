01e89fac826cf8c3506471df1a859886d29db2c1
napalm255 <napalm255@gmail.com>
2016-11-14 12:08:00 -0500

    expanded tests

--- tests/test_oal.py
+++ tests/test_oal.py
@@ -16,16 +16,31 @@ class TestOal(object):
         pass
 
     def test_command_line_interface(self):
+        """Test command line interface."""
         runner = CliRunner()
         result = runner.invoke(cli)
         assert result.exit_code == 0
         assert 'Office 365' in result.output
+
+    def test_help(self):
+        """Test help."""
+        runner = CliRunner()
         help_result = runner.invoke(cli, ['--help'])
         assert help_result.exit_code == 0
         assert 'Show this message and exit.' in help_result.output
 
-    def test_something(self):
-        pass
+    def test_options(self):
+        """Test options."""
+        runner = CliRunner()
+        product_url_result = runner.invoke(cli, ['-u', 'o365'])
+        assert product_url_result.exit_code == 0
+        assert 'Selected Products: o365' in product_url_result.output
+        product_v4_result = runner.invoke(cli, ['-4', 'o365'])
+        assert product_v4_result.exit_code == 0
+        assert 'Selected Products: o365' in product_v4_result.output
+        product_v6_result = runner.invoke(cli, ['-6', 'eop'])
+        assert product_v6_result.exit_code == 0
+        assert 'Selected Products: eop' in product_v6_result.output
 
     @classmethod
     def teardown_class(cls):
