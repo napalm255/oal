6aa992efb717290f62939e869f1ca621dc913839
napalm255 <napalm255@gmail.com>
2016-11-14 18:46:11 -0500

    Bump version: 0.3.4 → 0.3.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='oal',
-    version='0.3.4',
+    version='0.3.5',
     description="Office 365 Address Lists",
     long_description=readme + '\n\n' + history,
     author="Brad Gibson",
