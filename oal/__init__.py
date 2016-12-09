b8ec08cc69e9eac3acfb956c1b507f4410018420
napalm255 <napalm255@gmail.com>
2016-12-09 15:06:18 -0500

    Bump version: 0.3.9 → 0.3.10

--- oal/__init__.py
+++ oal/__init__.py
@@ -14,7 +14,7 @@ __title__ = 'Office 365 Address Lists'
 __summary__ = 'Office 365 Address Lists'
 __uri__ = 'https://github.com/napalm255/Office 365 Address Lists'
 
-__version__ = '0.3.9'
+__version__ = '0.3.10'
 
 __author__ = 'Brad Gibson'
 __email__ = 'napalm255@gmail.com'
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='oal',
-    version='0.3.9',
+    version='0.3.10',
     description="Office 365 Address Lists",
     long_description=readme + '\n\n' + history,
     author="Brad Gibson",
